aaf2fd895e8b60312f5fd3a2b4e687a62ce97031
Konrad <kojak334@gmail.com>
2020-06-14 22:34:34 +0100

    Update config

--- config.js
+++ config.js
@@ -5,34 +5,93 @@ const config = {
   "default_url": "https://duckduckgo.com/?q=${query}",
 
   "shortcuts": {
-    "r": {
-      "url": "https://old.reddit.com",
-      "default_url": "https://old.reddit.com/search?q=${query}"
+    "g": {
+      "url": "https://google.co.uk/",
+      "default_url": "https://google.com/search?q=${query}"
     },
 
-    "y": {
-      "url": "https://www.youtube.com",
-      "shortcuts": {
-        "s": {
-          "url": "https://www.youtube.com/feed/subscriptions",
-          'default_url': 'baz/${query}',
-          "shortcuts": {
-            "f": {
-              "url": "${y}/f",
-              'default_url': 'foo/${query}'
-            }
-          }
-        },
+    "d": {
+      "url": "https://duckduckgo.com/",
+      "default_url": "https://duckduckgo.com/?q=${query}"
+    },
 
-        "t": {
-          "url": "https://www.youtube.com/feed/trending",
-          "shortcuts": {
-            "z": {
-              "url": "memer"
-            }
-          }
+    "r": {
+      "url": "https://reddit.com",
+      "default_url": "https://reddit.com/search?q=${query}",
+      "shortcuts": {
+        "d": {
+          "url": "https://reddit.com/r/druggardening"
+        },
+        "g": {
+          "url": "https://reddit.com/r/gwent"
+        },
+        "a": {
+          "url": "https://reddit.com/r/anime"
+        },
+        "m": {
+          "url": "https://reddit.com/r/manga"
+        },
+        "ap": {
+          "url": "https://reddit.com/r/animepiracy"
+        },
+        "w": {
+          "url": "https://reddit.com/r/witcher"
         }
       }
+    },
+
+    "n": {
+      "url": "https://nyaa.si/",
+      "default_url": "https://nyaa.si/?f=0&c=1_2&s=seeders&o=desc&q=${query}"
+    },
+
+    "a": {
+      "url": "https://anilist.co/",
+      "default_url": "https://anilist.co/search/anime?search=${query}"
+    },
+
+    "m": {
+      "url": "https://mangadex.org/",
+      "default_url": "https://mangadex.org/search?title=${query}"
+    },
+
+    "1x": {
+      "url": "https://1337x.to",
+      "default_url": "https://1337x.to/search/${query}/1/"
+    },
+
+    "e": {
+      "url": "https://ebay.co.uk",
+      "default_url": "https://ebay.co.uk/sch/i.html?_nkw=${query}"
+    },
+
+    "nx": {
+      "url": "https://netflix.com"
+    },
+
+    "gh": {
+      "url": "https://github.com"
+    },
+
+    "gm": {
+      "url": "https://mail.google.com"
+    },
+
+    "s": {
+      "url": "http://sharetheseeds.me/forum"
+    },
+
+    "aw": {
+      "url": "https://aniwatch.me/",
+      "default_url": "https://aniwatch.me/search?q=${query}"
+    },
+
+    "q": {
+      "url": "https://qmail.qub.ac.uk"
+    },
+
+    "ra": {
+      "url": "https://rarbg.to"
     }
   }
 };
